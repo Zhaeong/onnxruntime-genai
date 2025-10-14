5965d7eae2c926d29115fc36cdfb46bc487e27d2
Baiju Meswani <bmeswani@microsoft.com>
2025-10-14 11:43:06 -0700

    Show a simple example

---
+++ examples/python/model-qa-simple.py
@@ -0,0 +1,83 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import argparse
+import json
+
+import onnxruntime_genai as og
+
+
+def run(args: argparse.Namespace):
+    config = og.Config(args.model_path)
+    config.clear_providers()
+    if args.execution_provider != "cpu":
+        config.append_provider(args.execution_provider)
+
+    model = og.Model(config)
+    tokenizer = og.Tokenizer(model)
+    streaming_tokenizer = tokenizer.create_stream()
+    params = og.GeneratorParams(model)
+    params.set_search_options(
+        do_sample=False,
+        max_length=2048,
+    )
+    generator = og.Generator(model, params)
+
+    messages = [{"role": "system", "content": ""}]
+    while prompt := input("🫵  : "):
+        if prompt == "/exit":
+            break
+
+        messages += [{"role": "user", "content": f"{prompt}"}]
+        messages = json.dumps(messages)
+
+        input_ids = tokenizer.encode(
+            tokenizer.apply_chat_template(
+                messages=messages, add_generation_prompt=True
+            )
+        )
+
+        generator.append_tokens(input_ids)
+        messages = []
+
+        print(f"🤖 :", end="", flush=True)
+
+        while not generator.is_done():
+            generator.generate_next_token()
+            new_token = generator.get_next_tokens()[0]
+            print(streaming_tokenizer.decode(new_token), end='', flush=True)
+
+        print()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="End-to-end AI Question/Answer example for gen-ai",
+    )
+    parser.add_argument(
+        "-m",
+        "--model_path",
+        type=str,
+        required=True,
+        help="Onnx model folder path (must contain genai_config.json and model.onnx)",
+    )
+    parser.add_argument(
+        "-e",
+        "--execution_provider",
+        type=str,
+        required=True,
+        choices=["cpu", "cuda", "dml", "webgpu"],
+        help="Execution provider to run ONNX model with",
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+
+    args = parser.parse_args()
+    if args.debug:
+        og.set_log_options(
+            enabled=True,
+            model_input_values=True,
+            model_output_values=True,
+            model_output_shapes=True,
+        )
+
+    run(args)
